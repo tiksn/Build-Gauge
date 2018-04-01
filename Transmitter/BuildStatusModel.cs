ea9d5f45cc2dcecaf36057ad7eab2753d8b50ae5
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-04-01 03:05:03 +0300

    state and output kind properties added

--- Transmitter/BuildStatusModel.cs
+++ Transmitter/BuildStatusModel.cs
@@ -4,8 +4,33 @@
     {
         private byte _flags;
 
+        public byte Flags => _flags;
         public byte Index { get; set; }
 
-        public byte Flags => _flags;
+        public BuildOutputKind OutputKind
+        {
+            get
+            {
+                return (BuildOutputKind)(_flags & 0b0000_1100);
+            }
+            set
+            {
+                _flags &= 0b1111_0011;
+                _flags &= (byte)((byte)value >> 2);
+            }
+        }
+
+        public BuildState State
+        {
+            get
+            {
+                return (BuildState)(_flags & 0b0000_0011);
+            }
+            set
+            {
+                _flags &= 0b1111_1100;
+                _flags &= (byte)value;
+            }
+        }
     }
 }
